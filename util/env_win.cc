159165214d34fa27c78bdfd56e07677f9425c283
Dylan K. Taylor <odigiman@gmail.com>
2018-12-07 18:58:26 +0000

    change MCPE_PLATFORM_WINDOWS_FAMILY to LEVELDB_PLATFORM_WINDOWS

--- util/env_win.cc
+++ util/env_win.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#ifdef MCPE_PLATFORM_WINDOWS_FAMILY
+#if defined(LEVELDB_PLATFORM_WINDOWS)
 
 #define VC_EXTRALEAN            // Exclude rarely-used stuff
 #define WIN32_LEAN_AND_MEAN     // Exclude rarely-used stuff from Windows headers
